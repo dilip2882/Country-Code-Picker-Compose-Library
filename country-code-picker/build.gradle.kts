f4d959e9b4dee222563bc569d63b0e377b556c1f
Dilip Patel <pateldilip2902@gmail.com>
2024-12-03 23:03:32 +0530

    modularization

--- country-code-picker/build.gradle.kts
+++ country-code-picker/build.gradle.kts
@@ -1,21 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.dilip.countrycodepickerexample"
-    compileSdk = 35
+    namespace = "com.dilip.country_code_picker"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.dilip.countrycodepickerexample"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,8 +37,6 @@ android {
 }
 
 dependencies {
-
-    implementation(project(":country-code-picker"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
